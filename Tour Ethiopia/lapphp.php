e34fd5bac641e670f9163f17be94b005c10e3624
kelkias44 <92637094+kelkias44@users.noreply.github.com>
2021-11-04 12:30:12 +0300

    Add files via upload

---
+++ Tour Ethiopia/lapphp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php example</title>
+    </head>
+      <body>
+          <?php
+
+
+
+
+          $x=5;
+          $y=6;
+          function product()
+          {
+               $GLOBALS 
+          }
+            ?>   
+      </body>
+</html>
